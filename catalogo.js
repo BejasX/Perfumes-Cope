7c30ca5a30fd1f8428f321dc2bfb8e0572de984e
Senzia <bejasminherman2009@gmail.com>
2025-06-25 00:00:15 -0300

    Update catalogo.js

--- catalogo.js
+++ catalogo.js
@@ -1,25 +1,25 @@
 const productos = [
   {
-    nombre: "Perfume Floral",
-    tipo: "floral",
-    presentacion: "50ml - Jazmín y rosa",
+    nombre: "Xerjoff – Erba Pura",
+    tipo: "Oriental",
+    presentacion: "50ml - Ámbar y vainilla",
     imagen: "img/Xerjoff.png",
     enlace: "productos/producto1.html"
   },
+  {
+    nombre: "Armaf - Mandarin Sky",
+    tipo: "Citrico",
+    presentacion: "100ml - Mandarina y menta",
+    imagen: "img/perfume2.jpg",
+    enlace: "productos/producto2.html"
+  }
   {
     nombre: "Perfume Floral",
     tipo: "floral",
     presentacion: "50ml - Jazmín y rosa",
     imagen: "img/perfume1.jpg",
-    enlace: "productos/producto2.html"
-  },
-  {
-    nombre: "Perfume Cítrico",
-    tipo: "citrico",
-    presentacion: "50ml - Limón y bergamota",
-    imagen: "img/perfume2.jpg",
     enlace: "productos/producto3.html"
-  }
+  },
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
